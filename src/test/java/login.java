9f151d9e5e8e03e4d879a527228185961b3a64a0
sreevalli <kotapatisreevalli@gmail.com>
2021-09-27 19:19:51 +0530

    initial commit

---
+++ src/test/java/login.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class login {
+	public static WebDriver driver;
+	@BeforeTest
+	public void setup() 
+	{ 
+	System.setProperty("webdriver.chrome.driver", "D:/Selenium Required/chromedriver_win/chromedriver.exe");
+	 driver = new ChromeDriver();
+	}
+	@Test
+	public void dologin()
+	{
+		
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("hi");
+		
+		driver.findElement(By.xpath("//input[@class='gNO89b']")).submit();	
+		}
+	
+	@AfterTest
+	public void teardown() throws Exception
+	{
+	Thread.sleep(1500);
+	driver.close();
+	}
+
+}
