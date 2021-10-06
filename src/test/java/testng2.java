3b87027ca09f7436bd5fd074fdf15fef4d236e2a
sreevalli <kotapatisreevalli@gmail.com>
2021-10-06 20:20:17 +0530

    by using assertion in 2nd testng class

--- src/test/java/testng2.java
+++ src/test/java/testng2.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class testng2 {
@@ -6,10 +7,15 @@ public class testng2 {
 	public void validatetitle() {
 		String expected_title="Yahoo.com";
 		String actual_title="Gmail.com";
-		if(expected_title.equals(actual_title))
+		/*if(expected_title.equals(actual_title))
 			System.out.println("pass");
 		else
-			System.out.println("fail");
+			System.out.println("fail");*/
+		//isElementPresent(xpath) true/false
+		//Assert.assertEquals(actual_title, expected_title);//it gives the report FAILED: validatetitle
+		//Assert.assertTrue(false,"element not found");//.AssertionError: element not found expected [true] but found [false]
+       // Assert.assertTrue(true, actual_title);
+		Assert.fail("in this test case elements are equal");
 	}
 
 }
